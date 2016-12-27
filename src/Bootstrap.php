00295aee1884c4504fed333aeda919d1cf97c7da
Kris <kris@meridian-ds.com>
2016-12-27 17:02:21 -0600

    adding the client directory

--- src/Bootstrap.php
+++ src/Bootstrap.php
@@ -60,7 +60,8 @@ class Bootstrap {
     $dir = shell_exec('pwd');
     $dir_parts = explode(DIRECTORY_SEPARATOR, $dir);
     $project = trim(array_pop($dir_parts));
-    $dir = $dir . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'project' . DIRECTORY_SEPARATOR . $project;
+    $client = trim(array_pop($dir_parts));
+    $dir = $dir . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'project' . DIRECTORY_SEPARATOR . $client . DIRECTORY_SEPARATOR . $project;
     $environment_file = file_get_contents($dir . DIRECTORY_SEPARATOR . 'environment.yml');
     if (!$environment_file) {
       throw new MissingEnvironmentConfigurationException("The environment configuration is missing, please contact your administrator.");
